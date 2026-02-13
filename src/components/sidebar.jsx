b60db74467958771c64b13e5129f69fe33ef7812
azizu06 <ab725492@ucf.edu>
2026-02-13 18:52:41 -0500

    refactor duplicate styling

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -6,26 +6,55 @@ import {
   GraduationCap,
   Wrench,
 } from "lucide-react";
+import { Personal, Education, Work, Projects, Skills } from "./Panels";
 
-export function Sidebar() {
-  const [active, setActive] = useState("personal");
+export const Sidebar = ({ activePanel, setPanel }) => {
   const panels = {
-    personal: <Personal />,
-    education: <Education />,
-    experience: <Experience />,
-    projects: <Projects />,
-    skills: <Skills />,
+    personal: Personal,
+    education: Education,
+    work: Work,
+    projects: Projects,
+    skills: Skills,
   };
+  const itemBase = "flex item-center w-full border p-6";
+  const inactive = "hover:bg-slate-900";
+  const active = "bg-blue-600";
+  const Panel = panels[activePanel];
   return (
-    <>
-      <div className="flex flex-col">
-        <User onClick={() => setActive("personal")} />
-        <GraduationCap onClick={() => setActive("education")} />
-        <Briefcase onClick={() => setActive("experience")} />
-        <FolderGit onClick={() => setActive("projects")} />
-        <Wrench onClick={() => setActive("skills")} />
+    <div className="flex gap-10">
+      <div className="flex flex-col h-screen border-r border-green-700 pt-10">
+        <button
+          className={`${itemBase} ${activePanel === "personal" ? active : inactive}`}
+          onClick={() => setPanel("personal")}
+        >
+          <User />
+        </button>
+        <button
+          className={`${itemBase} ${activePanel === "education" ? active : inactive}`}
+          onClick={() => setPanel("education")}
+        >
+          <GraduationCap />
+        </button>
+        <button
+          className={`${itemBase} ${activePanel === "work" ? active : inactive}`}
+          onClick={() => setPanel("work")}
+        >
+          <Briefcase />
+        </button>
+        <button
+          className={`${itemBase} ${activePanel === "projects" ? active : inactive}`}
+          onClick={() => setPanel("projects")}
+        >
+          <FolderGit />
+        </button>
+        <button
+          className={`${itemBase} ${activePanel === "skills" ? active : inactive}`}
+          onClick={() => setPanel("skills")}
+        >
+          <Wrench />
+        </button>
       </div>
-      <div className="flex flex-col">{panels[active]}</div>
-    </>
+      <div className="flex flex-col">{Panel && <Panel />}</div>
+    </div>
   );
-}
+};
