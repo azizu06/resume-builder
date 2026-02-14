ebb1212baef56d9b4d38f6d87795ba49bdc3b246
azizu06 <ab725492@ucf.edu>
2026-02-14 16:54:18 -0500

    fix style on main app

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,20 @@ import { Resume } from "./components/Resume";
 
 export const App = () => {
   const [resume, setResume] = useState(defaultResume);
-  const [acitvePanel, setActivePanel] = useState("");
+  const [activePanel, setActivePanel] = useState("");
+
   return (
-    <div className="flex min-h-screen w-screen border border-red-400">
-      <aside className="h-screen border-r border-blue-700 shrink-0">
-        <Sidebar activePanel={acitvePanel} setPanel={setActivePanel} />
+    <div className="flex min-h-screen w-screen border bg-black-700">
+      <aside className="border-r border-blue-700 shrink-0">
+        <Sidebar
+          activePanel={activePanel}
+          setPanel={setActivePanel}
+          resume={resume}
+          setResume={setResume}
+        />
       </aside>
-      <main className="flex-1 grid place-items-end border border-orange-700 p-10">
-        <Resume resume={resume} setResume={setResume} />
+      <main className="flex-1 grid place-items-start border border-orange-700 p-10">
+        <Resume resume={resume} />
       </main>
     </div>
   );
