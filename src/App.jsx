7adf5ad7a71e2af0fb4026e3b4baf2507a693725
azizu06 <ab725492@ucf.edu>
2026-02-15 03:12:57 -0500

    fix build error

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Sidebar } from "./components/Sidebar";
+import { Sidebar } from "./components/sidebar";
 import { defaultResume } from "./const";
 import { Resume } from "./components/Resume";
 
